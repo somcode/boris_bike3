b3ead602a1b06dff731e5f19101696a605bf4c22
Somayeh <mail.somayeh@gmail.com>
2018-02-24 22:07:15 +0000

    adding attr_reader and dock(bike) method and their test

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,4 +4,18 @@ class DockingStation
   def release_bike
     Bike.new
   end
+
+  #Use an instance variable to store the bike
+  # in the 'state' of this instance
+  def dock(bike)
+    @bike = bike
+  end
+
+
+  # let's add to our #bike method to return the
+  # bike we docked
+  # def bike
+  #   @bike
+  # end
+  attr_reader :bike
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -11,4 +11,18 @@ describe DockingStation do
       bike = subject.release_bike
       expect(bike).to be_working
     end
+
+    #it { is_expected.to respond_to(:dock).with(1).argument }
+     it 'docks something'do
+       bike = Bike.new
+       expect(subject.dock(bike)).to eq(bike)
+     end
+    #it { is_expected.to respond_to(:bike) }
+    it 'returns docked bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      # we need to return the bike we just docked
+      expect(subject.bike).to eq(bike)
+    end
+
 end
